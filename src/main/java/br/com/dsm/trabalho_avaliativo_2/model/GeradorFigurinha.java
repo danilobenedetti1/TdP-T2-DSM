7455b9848adfeea2d2ddffcddf53ac819477131e
Danilo Benedetti <danilobenedetti1@gmail.com>
2023-11-24 00:12:33 -0300

    ajustes

--- src/main/java/br/com/dsm/trabalho_avaliativo_2/model/GeradorFigurinha.java
+++ src/main/java/br/com/dsm/trabalho_avaliativo_2/model/GeradorFigurinha.java
@@ -3,7 +3,6 @@ package br.com.dsm.trabalho_avaliativo_2.model;
 import javax.imageio.ImageIO;
 import java.awt.Color;
 import java.awt.Font;
-import java.awt.FontMetrics;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 import java.io.File;
@@ -19,29 +18,21 @@ public class GeradorFigurinha {
 		// criar um nova imagem em memória com transparência e com tamanho novo
 		int largura = imagemOriginal.getWidth();
 		int altura = imagemOriginal.getHeight();
-		int novaAltura = altura + 200;
+		int novaAltura = altura + 100;
 		BufferedImage novaImagem = new BufferedImage(largura, novaAltura, BufferedImage.TYPE_INT_ARGB);
 
 		// copiar a imagem original para a nova imagem (em memória)
 		Graphics2D graphics = (Graphics2D) novaImagem.getGraphics();
 		graphics.drawImage(imagemOriginal, 0 , 0, null);
 
-		// configurar a fonte para escrever na imagem
-		var fonte = new Font(Font.SANS_SERIF, Font.BOLD, 100);
+		// escrever uma mensagem na nova imagem
+		var fonte = new Font(Font.SANS_SERIF, Font.BOLD, 35);
 		graphics.setFont(fonte);
 		graphics.setColor(Color.YELLOW);
-
-		// centralizar o título na imagem
-		FontMetrics fontMetrics = graphics.getFontMetrics();
-		int tituloLargura = fontMetrics.stringWidth(titulo);
-		int tituloAltura = fontMetrics.getHeight();
-		int tituloX = (largura - tituloLargura) / 2;
-		int tituloY = novaAltura - (tituloAltura / 2);
-
-		// escrever uma mensagem na nova imagem
-		graphics.drawString(titulo, tituloX, tituloY);
+		graphics.drawString(titulo, 50, novaAltura - 80);
 
 		String urlFigurinha = "saida/" + nomeArquivo + ".png";
+		// escrever uma mensagem na nova imagem
 		ImageIO.write(novaImagem, "png", new File(urlFigurinha));
 	}
 }
